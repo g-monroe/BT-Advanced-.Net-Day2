06beabdb459ed7b457bdf15774b210610c620679
Dan Drews <dan.drews@buildertrend.com>
2019-05-15 14:16:07 -0500

    Updated

--- Demo/WebApiDemo/Controllers/ToDosController.cs
+++ Demo/WebApiDemo/Controllers/ToDosController.cs
@@ -14,9 +14,17 @@ namespace WebApiDemo.Controllers
     {
 
         [HttpGet]
-        public ResponseObjects.ToDoList GetToDoList()
+        public ResponseObjects.ToDoList GetToDoList(string title = "", System.DateTime? date = null)
         {
             List<ToDo> todos = Datastore.ToDoHandler.GetToDos();
+            if (title != "")
+            {
+                todos = todos.Where(x => x.Title.Contains(title)).ToList();
+            }
+            if(date != null)
+            {
+                todos = todos.Where(x => x.DueDate == date).ToList();
+            }
             var resp = new ResponseObjects.ToDoList();
             resp.TotalCount = todos.Count;
             resp.Items = todos.Select(x => new ResponseObjects.ToDoListItem()
@@ -26,6 +34,36 @@ namespace WebApiDemo.Controllers
             }).ToList();
 
             return resp;
+        }
+
+        [HttpGet("{toDoId}")]
+        public ToDo GetToDo(int toDoId)
+        {
+            return WebApiDemo.Datastore.ToDoHandler.GetToDo(toDoId);
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteToDo(int id)
+        {
+            // Imagine we deleted it
+        }
+
+        [HttpPut("{id}/complete")]
+        public ToDo CompleteToDo(int id)
+        {
+            var todo = Datastore.ToDoHandler.GetToDo(id);
+            todo.Status = Datastore.Enums.ToDoEnums.Status.Complete;
+            Datastore.ToDoHandler.UpdateToDo(todo);
+            return todo;
+        }
+
+        [HttpPut("{id}")]
+        public ToDo UpdateToDo([FromRoute]int id, [FromBody]RequestObjects.ToDoCreationRequest request)
+        {
+            var todo = Datastore.ToDoHandler.GetToDo(id);
+            todo.Title = request.Title;
+            Datastore.ToDoHandler.UpdateToDo(todo);
+            return todo;
         }
 
         [HttpPost]
